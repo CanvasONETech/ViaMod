6aad64b07e2fc0a9b3b34088f50b02cce7739a5f
Canvas <129669414+CanvasONETech@users.noreply.github.com>
2026-06-28 08:41:54 -0400

    Update ModMenuIntegration to use empty list

--- src/main/java/com/byionic/viamod/ModMenuIntegration.java
+++ src/main/java/com/byionic/viamod/ModMenuIntegration.java
@@ -2,13 +2,13 @@ package com.byionic.viamod;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
 
-@Environment(EnvType.CLIENT)
+import java.util.Collections;
+
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> new ViaModScreen(parent);
+        return parent -> new ViaModScreen(parent, Collections.emptyList());
     }
 }
